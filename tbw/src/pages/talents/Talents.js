595998b63c19b4fd97cf9e0b82cb02177be6281e
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2024-01-03 17:59:02 +0100

    TBW-52 talents bg and bloc text

--- tbw/src/pages/talents/Talents.js
+++ tbw/src/pages/talents/Talents.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 import "./Talents.css";
+import BlocText from '../../components/blockText/BlocText';
 
 
 function Talents(props) {
+    const blocTexTitle = [
+        {
+            title: "Talents",
+            subtitle: "Graine de génie.",
+        }
+    ]
     return (
         <div className='bg-talents'>
-            talents
+           {blocTexTitle && (
+                <div>
+                    {blocTexTitle.map((value, i) => (
+                        <div key={i} className="col-lg-12 d-flex justify-content-center">
+                            <div>
+                                <BlocText key={i} title={value.title} subtitle={value.subtitle} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
