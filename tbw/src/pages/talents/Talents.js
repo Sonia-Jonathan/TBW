90ccc449ebc4ea0e750f385adcabcd82299ebf82
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2024-01-05 16:24:16 +0100

    TBW-40 add images talents

--- tbw/src/pages/talents/Talents.js
+++ tbw/src/pages/talents/Talents.js
@@ -12,8 +12,8 @@ function Talents(props) {
         }
     ]
     return (
-        <div className='bg-talents'>
-           {blocTexTitle && (
+        <div className='bg-talents pb-5'>
+            {blocTexTitle && (
                 <div>
                     {blocTexTitle.map((value, i) => (
                         <div key={i} className="col-lg-12 d-flex justify-content-center">
@@ -24,6 +24,24 @@ function Talents(props) {
                     ))}
                 </div>
             )}
+
+            <div className='mt-5 pb-5 d-flex justify-content-center col-lg-12'>
+                <div className='d-flex  justify-content-around col-lg-9'>
+                    <div className='col-lg-3'>
+                        <img className='img-talents' src='https://img.freepik.com/photos-premium/portrait-jeune-femme-mexicaine_79390-494.jpg' alt='' />
+                    </div>
+
+                    <div className='col-lg-3'>
+                        <img className='img-talents' src='https://us.123rf.com/450wm/nuevoimg/nuevoimg2309/nuevoimg230902059/212246214-portrait-d-un-beau-jeune-homme-avec-barbe-et-moustache-mode-beaut%C3%A9-pour-hommes.jpg?ver=6' alt='' />
+                    </div>
+
+                    <div className='col-lg-3'>
+                        <img className='img-talents' src='https://egeriephotographies.com/wp-content/uploads/2020/04/Femme.jpg' alt='' />
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 }
