37249b360916bec06165b2422acf1d6d1d9c4d4b
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2024-01-05 17:15:12 +0100

    TBW-50 TBW-29 TBW-30 css header change when scrolling, fixed footer at the bottom

--- tbw/src/components/header/Header.js
+++ tbw/src/components/header/Header.js
@@ -3,10 +3,24 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import "./Header.css";
+import { useState } from "react";
 
 function Header(props) {
+
+  const [color, setColor] = useState(false);
+  const changeColor = () =>{
+    if(window.scrollY>=30){
+      setColor(true);
+    }else{
+      setColor(false);
+    }
+  }
+
+  window.addEventListener('scroll', changeColor);
+
+
   return (
-    <Navbar expand="lg"  className="header">
+    <Navbar expand="lg"  className={`header ${color ? 'header-bg':''}`}>
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
