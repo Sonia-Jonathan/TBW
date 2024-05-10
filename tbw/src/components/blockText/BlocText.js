2109408d1e7a1dd23dc2c44811ea1e74ad1a9b2a
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2024-05-10 18:07:31 +0200

    fix btn regarder vision

--- tbw/src/components/blockText/BlocText.js
+++ tbw/src/components/blockText/BlocText.js
@@ -8,12 +8,16 @@ function BlocText(props) {
     const title = props.title
     const subtitle = props.subtitle
     const link = props.link
+    const btn = props.btn
     const btnTitle = props.btnTitle
     const btnFunction = props.btnFunction
 
     const handleClick = () => {
         if (btnFunction) {
             btnFunction();
+        }else if(btn.function){
+            console.log("btn.function", btn.function)
+            btn.function();
         }
     };
 
@@ -23,11 +27,12 @@ function BlocText(props) {
             <h1 className='white'>{title}</h1>
             <h2 className='white'>{subtitle}</h2>
             {link && (<a href={link.href} className='text-decoration-none blue'>{link.title}</a>)}
-            {btnFunction && (
+            {btnFunction || btn && (
                 <div className='mt-2'>
-                    <a  onClick={handleClick}><Button variant="outline-light" className='btn px-4'>{btnTitle}</Button></a>
+                    <a  onClick={handleClick}><Button variant="outline-light" className='btn px-4'>{btnTitle ? btnTitle : btn.title}</Button></a>
                 </div>
             )}
+            
             
         </div>
     );
