487a1b91a5897486755aabafc3d155a8143aa31e
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2024-09-01 18:25:38 +0200

    card talent slider

--- tbw/src/components/imgTalents/ImgTalents.js
+++ tbw/src/components/imgTalents/ImgTalents.js
@@ -1,14 +1,16 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import "./ImgTalents.css";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 
 function ImgTalents(props) {
+    const dataSrc = props.dataSrc;
+    const [flippedIndex, setFlippedIndex] = useState(null);
 
-    const dataSrc = props.dataSrc
+    const handleCardClick = (index) => {
+        setFlippedIndex(index === flippedIndex ? null : index);
+    };
 
     let settings = {
         dots: true,
@@ -18,51 +20,55 @@ function ImgTalents(props) {
         slidesToScroll: 1,
         centerMode: true,
         centerPadding: '0',
-
     };
 
     return (
-
-        <div className='mt-5 col-lg-12 '>
+        <div className='mt-5 col-lg-12'>
             {dataSrc && (
                 <div>
-                    <div className='talents-img text-center d-flex justify-content-around '>
+                    <div className='talents-img text-center d-flex justify-content-around'>
                         {dataSrc.map((value, i) => (
-                            <div key={i} className=" m-auto col-lg-12 flip-container ">
-                                <div className="flipper ">
+                            <div key={i} className={`m-auto col-lg-12 flip-container ${flippedIndex === i ? 'flip' : ''}`}>
+                                <div className="flipper">
                                     <div className="front col-lg-12">
-                                        {/* Contenu avant (image) */}
                                         <img className='frontRadius' src={value.src} alt={value.src} />
                                     </div>
-                                    <div className="back col-lg-12 ">
-                                        <p className=" col-lg-12 ">Nom : {value.nom}</p>
-                                        <p className=" col-lg-12 ">Prénom : {value.prenom}</p>
-                                        <p className=" col-lg-12 ">Description :  {value.description}</p>
+                                    <div className="back col-lg-12">
+                                        <p className="col-lg-12">Nom : {value.nom}</p>
+                                        <p className="col-lg-12">Prénom : {value.prenom}</p>
+                                        <p className="col-lg-12">Description : {value.description}</p>
                                     </div>
                                 </div>
                             </div>
                         ))}
-
                     </div>
 
                     <div className='m-auto talents-slider col-8'>
                         <Slider {...settings}>
                             {dataSrc.map((value, i) => (
-                                <div key={i} className='slider-image-container col-12'>
-                                    <img className='col-12 slider-image' src={value.src} alt={value.nom} />
-                                    <button className='card-button'> En savoir plus ...</button>
+                                <div key={i} className='slider-image-container'>
+                                    <div className={`slider-flipper ${flippedIndex === i ? 'flip' : ''}`}>
+                                        <div className="slider-front" onClick={() => handleCardClick(i)}>
+                                            <img className='slider-image' src={value.src} alt={value.nom} />
+                                            <div className='bg-blur'></div>
+                                            <button className='card-button' onClick={() => handleCardClick(i)} >
+                                                En savoir plus ...
+                                            </button>
+                                        </div>
+                                        <div className="back text-center slider-back" onClick={() => handleCardClick()}>
+                                            <p className="col-12">Nom : {value.nom}</p>
+                                            <p className="col-12">Prénom : {value.prenom}</p>
+                                            <p className="col-12">Description : {value.description}</p>
+                                        </div>
+                                    </div>
+
                                 </div>
                             ))}
                         </Slider>
                     </div>
                 </div>
-
             )}
-
-
         </div>
-
-
     );
 }
 
